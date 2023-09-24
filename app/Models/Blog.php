37677e015ca9a8846cd9bf7027088e2bb585decf
KyawZayYa <kyawzayya100417@gmail.com>
2023-09-24 13:54:32 +0630

    blog_category & movie_category CRUD

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\BlogCategory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Blog extends Model
 {
@@ -12,5 +13,10 @@ class Blog extends Model
     protected $fillable = [
         'title',
         'content',
+        'category_id',
     ];
+
+    public function blogCategory() {
+        return $this->belongsTo(BlogCategory::class,'category_id');
+    }
 }
