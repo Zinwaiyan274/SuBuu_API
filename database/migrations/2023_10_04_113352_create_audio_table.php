dea729543950940644d4c5ed81702c56a978b3ef
Thu Min <minnt5637@gmail.com>
2023-10-04 18:08:54 +0630

    migration for Music Feature

---
+++ database/migrations/2023_10_04_113352_create_audio_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('audio', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->string('audio');
+            $table->string('audio_title');
+            $table->text('artist_id');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('audio');
+    }
+};
