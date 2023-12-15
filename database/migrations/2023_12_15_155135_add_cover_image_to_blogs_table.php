ad0049e1df51ded7a209665af491158d0918ec3a
KyawZayYa <kyawzayya100417@gmail.com>
2023-12-15 22:29:35 +0630

    blog add cover_image update

--- database/migrations/2023_12_15_155135_add_cover_image_to_blogs_table.php
+++ database/migrations/2023_12_15_155135_add_cover_image_to_blogs_table.php
@@ -13,12 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('blogs', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->longText('content');
-            $table->integer('category_id');
-            $table->timestamps();
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->string('cover_image')->after('title');
         });
     }
 
@@ -29,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blogs');
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn('phone_number');
+        });
     }
 };
